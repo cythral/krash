4d1b2ad31ddf6f6731678dd51dd19d2dc44b2696
Talen Fisher <tqfisher@iastate.edu>
2019-03-20 10:54:57 -0500

    let host handle https instead

--- Startup.cs
+++ Startup.cs
@@ -50,13 +50,8 @@ namespace krash {
 
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
-
-            } else {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
+            
             app.UseMvc();
         }
     }
